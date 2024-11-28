4fac8339e6a412cbb462cead675811133b2e9c69
Gabriel Teixeira <gabriel.melo@gd3.com.br>
2024-11-28 02:14:51 -0300

    refine game experience

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
-randomNumber = generateRandomNumber(10);
-let attemptNumber = 1;
-let newGame = document.getElementById('restart').value;
+let drawnNumbers = []; // drawnNumbersList
+let attemptNumber = 1; // Numbers of attempt
+let lastNumber = 10; // Range of random numbers
+randomNumber = generateRandomNumber(lastNumber); // Generate a random number
+
 displayWelcomeMessage();
+
+// generate a random number between 1 and 10
+function generateRandomNumber(maxNumber){
+    let randomNumber = parseInt((Math.random()*maxNumber)+1);
+    let drawnNumbersLength = drawnNumbers.length;
+    if(drawnNumbersLength > 3){
+        drawnNumbers.shift();
+    };
+    if(drawnNumbers.includes(randomNumber)){
+        return generateRandomNumber(maxNumber);
+    }else{
+        drawnNumbers.push(randomNumber);
+        return randomNumber;
+    }
+    return randomNumber;
+};
+
+// function onclick from the button guess
+function verifyGuess(){
+    let guess = parseInt(document.querySelector('input').value);
+    console.log(drawnNumbers);
+    console.log(randomNumber);
+    if(guess == randomNumber){
+        displayTextOnScreen('h1', 'Acertou');
+        let attemptWord = attemptNumber > 1 ? 'tentativas' : 'tentativa';
+        displayTextOnScreen('p', `O número secreto é ${randomNumber} e você acertou com ${attemptNumber} ${attemptWord}!`);
+        cleanInput();
+        document.getElementById('restart').removeAttribute('disabled');
+        document.getElementById('guess').setAttribute('disabled', true);
+        document.getElementById('guessInput').setAttribute('disabled', true);
+    }else{
+        if(guess > randomNumber){
+            displayTextOnScreen('p', 'O número secreto é menor que o número escolhido!');
+            attemptNumber++;
+        }else{
+            displayTextOnScreen('p', 'O número secreto é maior que o número escolhido!');
+            attemptNumber++;
+        }
+        displayTextOnScreen('h1', 'Errou');
+        cleanInput();
+    };
+};
+
+// function onclick from the buttom restart
+function restartGame(){
+    randomNumber = generateRandomNumber(lastNumber);
+    attemptNumber = 1;
+    displayWelcomeMessage();
+    document.getElementById('restart').setAttribute('disabled', true);
+    cleanInput();
+    document.getElementById('guess').removeAttribute('disabled');
+    document.getElementById('guessInput').removeAttribute('disabled');
+};
 
 // display the first welcome message
 function displayWelcomeMessage(){
     displayTextOnScreen('h1', 'Jogo do número secreto');
-    displayTextOnScreen('p', 'Escolha um número entre 1 e 10');
+    displayTextOnScreen('p', `Escolha um número entre 1 e ${lastNumber}`);
 }
 
 // display the html text on screen
@@ -15,47 +70,8 @@ function displayTextOnScreen(tag, text){
     variable.innerHTML = text;
 };
 
-// generate a random number between 1 and 10
-function generateRandomNumber(maxNumber){
-    return parseInt((Math.random()*maxNumber)+1);
-};
-
 // clean input field
 function cleanInput(){
-    chute = document.querySelector('input');
-    chute.value = '';
+    guess = document.querySelector('input');
+    guess.value = '';
 };
-
-// function onclick from the button chute
-function verificarChute(){
-    let chute = parseInt(document.querySelector('input').value);
-    console.log(newGame);
-    if(chute == randomNumber){
-        displayTextOnScreen('h1', 'Acertou!');
-        let attemptWord = attemptNumber > 1 ? 'tentativas' : 'tentativa';
-        displayTextOnScreen('p', `O número secreto é ${randomNumber} e você acertou com ${attemptNumber} ${attemptWord}`);
-        cleanInput();
-        document.getElementById('restart').removeAttribute('disabled');
-    }else{
-        if(chute > randomNumber){
-            displayTextOnScreen('p', 'O número secreto é menor que o número escolhido!');
-            attemptNumber++;
-        }else{
-            displayTextOnScreen('p', 'O número secreto é maior que o número escolhido!');
-            attemptNumber++;
-        }
-        displayTextOnScreen('h1', 'Errou!');
-        cleanInput();
-    };
-};
-
-// function onclick from the buttom restart
-function restartGame(){
-    randomNumber = generateRandomNumber(10);
-    attemptNumber = 1;
-    displayWelcomeMessage();
-    document.getElementById('restart').setAttribute('disabled', true);
-    cleanInput();
-};
-
-
